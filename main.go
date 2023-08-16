c83e8da8249744270254b1f49fa200da5a5e1e5e
mjc2001 <mjchapman2001@gmail.com>
2023-08-16 12:39:09 +0100

    Recursive insertion sort

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	arr := []int{2, 9, 5, 4, 7, 10, 1, 6, 8, 3, 0}
 
-	sorting.InsertionSortI(arr)
+	sorting.InsertionSortR(arr, len(arr)-1)
 
 	fmt.Println(arr)
 }
--- pkg/insertion.go
+++ pkg/insertion.go
@@ -13,3 +13,18 @@ func InsertionSortI(arr []int) {
 		arr[j+1] = curr
 	}
 }
+
+func InsertionSortR(arr []int, n int) {
+	if n > 0 {
+		InsertionSortR(arr, n-1)
+		curr := arr[n]
+		j := n - 1
+
+		for j >= 0 && arr[j] > curr {
+			arr[j+1] = arr[j]
+			j -= 1
+		}
+
+		arr[j+1] = curr
+	}
+}
